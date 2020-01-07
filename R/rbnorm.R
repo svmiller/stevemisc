89452df362c0e4f3cacd278ca2f00afc98aad924
svmiller <steven.v.miller@gmail.com>
2020-01-07 14:34:38 -0500

    add rbnorm function.

---
+++ R/rbnorm.R
@@ -0,0 +1,14 @@
+rbnorm <- function(n,mean,sd,lowerbound,upperbound, round = FALSE) {
+  range <- upperbound - lowerbound
+  m <- (mean - lowerbound)/range
+  s <- sd/range
+  a <- (m^2 - m^3 - m*s^2)/s^2 # calculate alpha for rbeta
+  b <- (m-2*m^2+m^3-s^2+m*s^2)/s^2 # calculate beta for rbeta
+  data <- rbeta(n,a,b) # generate the data
+  data <- lowerbound + data * range # squeeze it within the bounds.
+  if (round == FALSE) {   return(data) }
+  if (round == TRUE) {
+    data <- round(data)
+    return(data)
+  }
+}
