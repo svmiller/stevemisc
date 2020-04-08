6fd14176515976e7d4316b2fe712eb5bab8625e0
svmiller <steven.v.miller@gmail.com>
2020-04-08 10:33:23 -0400

    improve get_sims(). Can now handle (g)lms too. Seed is optional now. Also a bit 'tidier' as well.

--- R/get_sims.R
+++ R/get_sims.R
@@ -1,19 +1,26 @@
 get_sims <- function(model, newdata, nsim, seed){
   # require(arm)
-  set.seed(seed)
-  arguments <- as.list(match.call())
+  if(missing(seed)) {
+
+  } else {
+    set.seed(seed)
+  }
   modelsim <- arm::sim(model, n.sims=nsim)
-  mm.dat = model.matrix(terms(model),newdata)
-  outputfull<-NULL
+  MM = model.matrix(terms(model),newdata)
+  Sims <- tibble(y = numeric(),
+                 sim = numeric())
   for(i in (1:nsim)) {
     output <- NULL
-    yi <- mm.dat %*% coef(modelsim)$fixef[i,]
+    if (is(model,"lm") == TRUE | is(model,"glm") == TRUE) {
+      yi <- MM %*% coef(modelsim)[i,] }
+    else { # assuming it's merMod
+      yi <- MM %*% coef(modelsim)$fixef[i,]
+    }
     sim<-rep(i, length (yi))
-    output<-cbind(yi, sim)
-    outputfull<-rbind (outputfull, output)
+    hold_me <- suppressMessages(as_tibble(cbind(yi, sim))) %>% rename(y = V1)
+    Sims <- bind_rows(Sims, hold_me)
   }
-    outputfull<- as_tibble(outputfull)
-names (outputfull)<-c("y", "sim")
-  return(outputfull)
+  return(Sims)
 
 }
+
