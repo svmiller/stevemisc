28c0db3dd3312bacfba5da6201638d506b9557c4
svmiller <steven.v.miller@gmail.com>
2021-10-07 09:51:53 -0400

    update

--- R/linloess_plot.R
+++ R/linloess_plot.R
@@ -10,35 +10,38 @@
 #' @details This function makes an implicit assumption that there is no variable in the regression
 #'  formula with the name ".y".
 #'
-#' @return \code{linloess_plot()} returns a faceted scatterplot as a \pkg{ggplot2} object. You can add cosmetic features
-#' to it after the fact.
+#' @return \code{linloess_plot()} returns a faceted scatterplot as a \pkg{ggplot2} object. The linear smoother is in solid blue (with blue
+#' standard error bands) and the LOESS smoother is a dashed black line (with gray/default standard error bands). You can add
+#' cosmetic features to it after the fact. The function may spit warnings to you related to the LOESS smoother, depending your data. I think
+#' these to be fine the extent to which this is really just a visual aid and an informal diagnostic for the linearity assumption.
 #'
 #' @author Steven V. Miller
 #'
 #' @param mod a fitted OLS model
+#' @param ... optional parameters, passed to the scatterplot (\code{geom_point()}) component of this function. Useful if you want to make the smoothers more legible against the points.
 #'
 #' @examples
 #'
 #' M1 <- lm(mpg ~ ., data=mtcars)
 #'
 #' linloess_plot(M1)
-#'
+#' linloess_plot(M1, color="black", pch=21)
 
 
-linloess_plot <- function(mod) {
+linloess_plot <- function(mod, ...) {
   modframe <- model.frame(mod)
 
   dat <- gather(modframe, "var", "value", 2:ncol(modframe))
   colnames(dat)[1] <- c(".y")
 
-  ggplot(dat, aes(value, .y)) +
+  ggplot(dat, aes(.data$value, .data$.y)) +
     # Create your facet now since the var variable is the particular x variable.
     # Make sure to set scale="free_x" because these x variables are all on different scales
-    facet_wrap(~var, scale="free_x") +
+    ggplot2::facet_wrap(~var, scale="free_x") +
     # scatterplot
-    geom_point() +
+    geom_point(...) +
     # linear smoother
-    geom_smooth(method="lm") +
+    geom_smooth(method="lm", fill="blue") +
     # loess smoother, with different color
     geom_smooth(method="loess", color="black", linetype="dashed")
 
