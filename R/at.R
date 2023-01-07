86f770388153316252ae988c86fe46d69f301d24
svmiller <steven.v.miller@gmail.com>
2023-01-07 11:21:48 +0100

    14 failed votes

--- R/at.R
+++ R/at.R
@@ -7,6 +7,13 @@
 #'
 #' @details
 #'
+#' \code{center_at} is a wrapper for \code{mutate_at} and \code{rename_at} from
+#' \pkg{dplyr}. It takes supplied vectors and effectively centers them from the
+#' mean. It then renames these new variables to have a prefix of \code{c_}. It
+#' is best used *without* a \code{group_by} preceding it. The underlying code is
+#' identical to \code{within_at} in this same package, though it results in new
+#' variables with a different prefix.
+#'
 #' \code{r1sd_at} is a wrapper for \code{mutate_at} and \code{rename_at} from
 #' \pkg{dplyr}. It both rescales the supplied vectors to new vectors and renames
 #' the vectors to each have a prefix of \code{s_}. Note the rescaling here is
@@ -17,20 +24,13 @@
 #' the vectors to each have a prefix of \code{z_}. Note the rescaling here is by
 #' two standard deviations and not one.
 #'
+#' \code{within_at} is a wrapper for \code{mutate_at} and \code{rename_at} from
+#' \pkg{dplyr}. It takes supplied vectors and effectively centers them from the
+#' mean. It then renames these new variables to have a prefix of \code{w_}. It
+#' is best used with a \code{group_by} preceding it on some kind of "subject",
+#' with an eye toward ultimately including them as covariates in a panel model
+#' to isolate so-called "within" effects.
 #'
-#'
-#' By default, `na.rm` is set to TRUE. If you have missing data, the function will just pass
-#' over them.
-#'
-#' Gelman (2008) argues that rescaling by two standard deviations puts regression inputs
-#' on roughly the same scale no matter their original scale. This allows for some honest, if preliminary,
-#' assessment of relative effect sizes from the regression output. This does that, but
-#' without requiring the \code{rescale} function from \pkg{arm}.
-#' I'm trying to reduce the packages on which my workflow relies.
-#'
-#' Importantly, I tend to rescale only the ordinal and interval inputs and leave the binary inputs as 0/1.
-#' So, my \code{r2sd} function doesn't have any of the fancier if-else statements that Gelman's \code{rescale}
-#' function has.
 #'
 #' @param data a data frame
 #' @param x a vector, likely in your data frame
@@ -39,23 +39,42 @@
 #' @param na what to do with NAs in the vector. Defaults to TRUE
 #' (i.e. passes over the missing observations). Not applicable to \code{diff_at}.
 #'
-#' @return The function returns a numeric vector rescaled with a mean of 0 and a
-#' standard deviation of .5.
-#'
-#' @references Gelman, Andrew. 2008. "Scaling Regression Inputs by Dividing by Two Standard Deviations." \emph{Statistics in Medicine} 27: 2865--2873.
+#' @return The function returns a set of new vectors in a data frame after
+#' performing relevant functions. The new vectors have distinct prefixes
+#' corresponding with the action performed on them.
 #'
 #' @examples
 #'
-#' data.frame(x = cumsum(rnorm(10)),
-#'            y = cumsum(rnorm(10)),
-#'            group = sample(c("A", "B"))) -> Example
+#' center_at(USArrests, c("Murder", "Assault"))
 #'
-#' # Note: this works in a group_by() context, which you should do in a case like this.
-#' diff_at(Example, c("x", "y"))
+#' diff_at(USArrests, c("Murder", "Assault"))
 #'
-#' r1sd_at(Example, c("x", "y"))
+#' r1sd_at(USArrests, c("Murder", "Assault"))
 #'
-#' r2sd_at(Example, c("x", "y"))
+#' r2sd_at(USArrests, c("Murder", "Assault"))
+#'
+#' # Note: best used with a group_by() preceding it.
+#' within_at(USArrests, c("Murder", "Assault"))
+
+#' @rdname at
+#' @export
+#'
+
+center_at <- function(data, x, na=TRUE) {
+
+  if(length(x) == 1) {
+    stop("The use of a scoped helper verb like this requires more than one variable.")
+  }
+
+  data %>%
+    mutate_at(vars(all_of(x)),
+              list(c = ~. - mean(., na = na))) %>%
+    rename_at(vars(contains("_c")),
+              ~paste("c", gsub("_c", "", .), sep = "_") ) -> data
+  return(data)
+
+}
+
 
 
 #' @rdname at
@@ -118,6 +137,25 @@ r2sd_at <- function(data, x, na=TRUE) {
               list(z = ~r2sd(., na=na))) %>%
     rename_at(vars(contains("_z")),
               ~paste("z", gsub("_z", "", .), sep = "_") ) -> data
+  return(data)
+
+}
+
+#' @rdname at
+#' @export
+#'
+
+within_at <- function(data, x, na=TRUE) {
+
+  if(length(x) == 1) {
+    stop("The use of a scoped helper verb like this requires more than one variable.")
+  }
+
+  data %>%
+    mutate_at(vars(all_of(x)),
+              list(w = ~. - mean(., na = na))) %>%
+    rename_at(vars(contains("_w")),
+              ~paste("w", gsub("_w", "", .), sep = "_") ) -> data
   return(data)
 
 }
