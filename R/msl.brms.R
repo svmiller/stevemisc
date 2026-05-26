c90f8c48a19dab2f8b650044175267ddaf74a76b
svmiller <steven.v.miller@gmail.com>
2026-05-26 16:14:48 +0200

    adsfs

--- R/msl.brms.R
+++ R/msl.brms.R
@@ -9,11 +9,17 @@
 #' @details
 #'
 #' Bayesians typically recoil at the thought of reporting *p*-values for
-#' presentation even as reviewers ask to see these.
+#' presentation even as reviewers ask to see these. The *p*-value in question is
+#' the "probability of direction" of an effect's existence. Makowski et al. (2019)
+#' is a worthwhile read on this procedure. The function calculates what they
+#' propose but doesn't require \pkg{bayestestR} as a dependency.
 #'
 #' Right now, the only easy goodness of fit parameter to return is the number of
 #' observations. The output of this function is easily customized ex post by the
 #' user.
+#'
+#' The function scrubs the `b_` prefix in the terms, which is is a convention of
+#' the modeling procedure in \pkg{brms}.
 #'
 #' @param mod a model fitted in \pkg{brms}
 #' @param robust logical, defaults to FALSE. If FALSE, the "tidy" estimates
@@ -26,6 +32,12 @@
 #' \code{msl.brms()} takes a model fitted by \pkg{brms} and prepares a custom
 #' \pkg{modelsummary} list for formatting with that package's `modelsummary()`
 #' function.
+#'
+#' @references
+#'
+#' Makowski, Dominique, Mattan S. Ben-Shachar, S. H. Annabel Chen, & Daniel
+#' Lüdecke. 2019.  "Indices of Effect Existence and Significance in the Bayesian
+#' Framework." *Frontiers in Psychology* (10)2767.
 #'
 #' @examples
 #'
